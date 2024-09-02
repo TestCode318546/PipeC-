ac5dc6772904464c1cad4ecac0b0522c63a95800
User <anocha_ping@hotmail.com>
2024-09-02 16:50:13 +0700

    add

---
+++ PipeClient/ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Named Pipe Client started...");
+
+            while (true)
+            {
+                using (var client = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut))
+                {
+                    client.Connect();
+
+                    using (var reader = new StreamReader(client))
+                    using (var writer = new StreamWriter(client) { AutoFlush = true })
+                    {
+                        string serverMessage = reader.ReadLine();
+                        Console.WriteLine("Received from server: " + serverMessage);
+                        writer.WriteLine("Hello from the client!");
+                    }
+                }
+
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
---
+++ PipeServer/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Named Pipe Server started...");
+
+            while (true)
+            {
+                using (var server = new NamedPipeServerStream("testpipe", PipeDirection.InOut))
+                {
+                    server.WaitForConnection();
+
+                    using (var reader = new StreamReader(server))
+                    using (var writer = new StreamWriter(server) { AutoFlush = true })
+                    {
+                        writer.WriteLine("Hello from the server!");
+                        string clientMessage = reader.ReadLine();
+                        Console.WriteLine("Received from client: " + clientMessage);
+                    }
+                }
+            }
+        }
+    }
+}
